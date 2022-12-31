1eed9d4586a47d425944f8877f1f04f516e4fe6d
Osnaelle <naellyfarias@gmail.com>
2022-12-31 08:40:03 -0300

    new changes

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+ let api_key= "4cb6b96a8e53003fd41145b83e779b89"
+
+ let img_url ="https://image.tmdb.org/t/p/w500"
+
+ let generos_list_api ="https://api.themoviedb.org/3/genre/movie/list?"
+
+ let movie_generos="https://api.themoviedb.org/3/discover/movie?"
